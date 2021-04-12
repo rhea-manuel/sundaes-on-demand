febf6fba64a68d5786de2cb210fc5fbec57c6870
Rhea Manuel <rheamanuel02@gmail.com>
2021-04-12 05:54:46 -0400

    fixed unused import

--- src/pages/summary/test/OrderSummary.test.js
+++ src/pages/summary/test/OrderSummary.test.js
@@ -1,4 +1,4 @@
-import { queryByText, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import SummaryForm from "../SummaryForm";
 
